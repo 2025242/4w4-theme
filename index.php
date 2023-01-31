f15fe1333520a396f4cbaa0f58d65d86f8e63df5
Bourakkadi Zarrouki <e2025242@cmaisonneuve.qc.ca>
2023-01-31 12:07:38 -0500

    Debut structure sass

--- index.php
+++ index.php
@@ -1,16 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>mon 4W4</title>
-    <?php wp_head() ?>
-</head>
-<body>
+<?php get_header(); ?>
     <main>
     <h1>Bienvenue sur 4W4</h1>   
     </main>
-    
+
+    <?php get_footer(); ?>
+
 </body>
 </html>
