85e8b4fb8530842d2c1aa34132e348440fd4f7d4
Bourakkadi Zarrouki <e2025242@cmaisonneuve.qc.ca>
2023-02-02 11:10:57 -0500

    s2c2 boucle word press wp

--- index.php
+++ index.php
@@ -4,6 +4,23 @@
     </main>
 
     <?php get_footer(); ?>
+<pre> front-page.php</pre>
+    <?php
+     if (have_posts()):
+       while (have_posts()):
+    
+        the_post(); //extraire un objet post?>
+        <article>
+        <h3><?php the_title('<h3>', '</h3>');//affiche le titre du post?></h3>
+       <h6> Extrait:</h6><?php the_excerpt();//show extrait ?> 
+       <h6> Contenu:</h6><?php the_content(); //afficher le contenu ?>
+       </article>
+
+     <?php endwhile;?>
+     <?php endif;?>
+    
+    ?>
+
 
 </body>
 </html>
