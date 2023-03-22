39111e809117f5ecc0102e122a77486f5052ab23
Salma Bourakkadi Zarrouki <bourakkadizarroukisalma@gamil.com>
2023-03-21 23:00:18 -0400

    s8c1 ajout footer

--- footer.php
+++ footer.php
@@ -1,7 +1,10 @@
-<footer class="site__footer">
+<footer class="site_footer">
     <section class="lien">Lien #1</section>
     <section class="lien">Lien #2</section>
     <section class="lien">Lien #3</section>
+</footer>
+<footer class="teste">
+<p class="texte_footer"> info | guide | cantacter </p>
 </footer>
         <?php wp_footer(); ?>
 </body>
